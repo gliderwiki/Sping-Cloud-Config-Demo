2a07e462d799b19dd9494bb4ee56b2f1291db840
yionlee <performizer@gmail.com>
2019-03-10 16:16:01 +0900

    config server

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,12 +1,12 @@
 package com.example.demo;
 
-import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 
 @SpringBootApplication
-@EnableEncryptableProperties
+@EnableConfigServer
 public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
